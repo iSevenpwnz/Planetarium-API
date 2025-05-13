c6086501730783fa95b5efc5fe92d0d11576d16d
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-05-14 00:40:29 +0300

    + set

--- config/settings.py
+++ config/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "planetarium_db"),
+        "USER": os.environ.get("POSTGRES_USER", "planetarium_user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "yoursecurepassword"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
